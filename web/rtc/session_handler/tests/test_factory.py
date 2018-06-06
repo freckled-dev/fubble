1575328c24f62c7810135a18faad2dd7bec0c486
Markus Lanner <contact@markus-lanner.com>
2018-06-06 18:02:42 +0200

    added test for app-factory

---
+++ web/rtc/session_handler/tests/test_factory.py
@@ -0,0 +1,10 @@
+from server import create_app
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello, World!'
+
