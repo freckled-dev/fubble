e833c83a0710a731c1bf257fbceb379c340db0d9
Markus Lanner <contact@markus-lanner.com>
2018-06-06 17:59:33 +0200

    initialised rtc server

---
+++ web/rtc/session_handler/tests/conftest.py
@@ -0,0 +1,27 @@
+import os
+import tempfile
+import pytest
+
+from server import create_app
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mktemp()
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
