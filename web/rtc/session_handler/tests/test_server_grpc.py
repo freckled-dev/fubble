0663c5a0be5804d2fb779e5b95e90e04396e8744
Markus Lanner <contact@markus-lanner.com>
2018-06-10 18:35:16 +0200

    fixed code style for proto file

--- web/rtc/session_handler/tests/test_server_grpc.py
+++ web/rtc/session_handler/tests/test_server_grpc.py
@@ -8,10 +8,10 @@ def test_construction_stub(rtc_stub):
 
 def test_join_room(rtc_stub):
     response = rtc_stub.JoinRoom(server.web_rtc_pb2.JoinRoomRequest(id="room id"))
-    assert response.rtcServer == "https://rtc.fubble.io/room id"
+    assert response.rtc_server == "https://rtc.fubble.io/room id"
 
 def test_delete_room(rtc_stub):
     response = rtc_stub.JoinRoom(server.web_rtc_pb2.JoinRoomRequest(id="room id"))
-    assert response.rtcServer == "https://rtc.fubble.io/room id"
+    assert response.rtc_server == "https://rtc.fubble.io/room id"
     no_response = rtc_stub.DeleteRoom(server.web_rtc_pb2.DeleteRoomRequest(id="room id"))
 
