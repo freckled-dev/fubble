ed18353407df5064ae084d891ed07981bda7f9a5
Tamrielle <manuela.weilharter@gmail.com>
2018-07-27 22:48:42 +0200

    preparing some more functions - settings, clickhandler for room list, ...

--- android/app/src/main/java/com/freckles/of/couple/fubble/tools/AppDatabase.java
+++ android/app/src/main/java/com/freckles/of/couple/fubble/tools/AppDatabase.java
@@ -8,5 +8,7 @@ import com.freckles.of.couple.fubble.entity.FubbleRoom;
 
 @Database(entities = {FubbleRoom.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+
     public abstract RoomDao roomDao();
+
 }
