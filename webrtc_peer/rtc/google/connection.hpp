add60c00ed93d2904638e6a3611c37681de7f6e1
Markus Lanner <contact@markus-lanner.com>
2019-11-17 11:54:13 +0100

    implemented google connection creator

--- webrtc_peer/rtc/google/connection.hpp
+++ webrtc_peer/rtc/google/connection.hpp
@@ -4,11 +4,39 @@
 #include "logging/logger.hpp"
 #include "rtc/connection.hpp"
 #include <api/candidate.h>
+#include <api/peer_connection_interface.h>
 
 namespace rtc::google {
-class connection : public rtc::connection {
+class connection : public rtc::connection,
+                   public ::webrtc::PeerConnectionObserver {
 public:
   ~connection() override;
+  void initialise(rtc::scoped_refptr<::webrtc::PeerConnectionInterface> native);
+  boost::future<session_description> create_offer() override;
+  boost::future<void>
+  set_local_description(const session_description &) override;
+  boost::future<void>
+  set_remote_description(const session_description &) override;
+  void add_track(track_ptr) override;
+
+protected:
+  void OnSignalingChange(
+      ::webrtc::PeerConnectionInterface::SignalingState new_state) override;
+  void
+  OnAddTrack(rtc::scoped_refptr<webrtc::RtpReceiverInterface> receiver,
+             const std::vector<rtc::scoped_refptr<webrtc::MediaStreamInterface>>
+                 &streams) override;
+  void OnDataChannel(::rtc::scoped_refptr<::webrtc::DataChannelInterface>
+                         data_channel) override;
+  void OnRenegotiationNeeded() override;
+  void OnIceGatheringChange(
+      webrtc::PeerConnectionInterface::IceGatheringState new_state) override;
+  void
+  OnIceCandidate(const ::webrtc::IceCandidateInterface *candidate) override;
+
+private:
+  logging::logger logger;
+  rtc::scoped_refptr<::webrtc::PeerConnectionInterface> native;
 };
 } // namespace rtc::google
 
