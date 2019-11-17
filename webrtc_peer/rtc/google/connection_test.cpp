e8815aeef4b8271a38874163dcfcbfc0d23fc326
Markus Lanner <contact@markus-lanner.com>
2019-11-17 14:44:30 +0100

    added data_channel to googles connection

--- webrtc_peer/rtc/google/connection_test.cpp
+++ webrtc_peer/rtc/google/connection_test.cpp
@@ -1,10 +1,42 @@
 #include "connection.hpp"
 #include "connection_creator.hpp"
+#include <boost/thread/executors/inline_executor.hpp>
 #include <fmt/format.h>
 #include <gtest/gtest.h>
+#include <thread>
 
-TEST(GoogleConnection, Instance) {
+namespace {
+struct GoogleConnection : ::testing::Test {
+  logging::logger logger;
+  boost::inline_executor executor;
   rtc::google::connection_creator creator;
   std::unique_ptr<rtc::google::connection> instance = creator();
+};
+struct wait_for_event {
+  std::mutex mutex;
+  wait_for_event() { mutex.lock(); }
+  void wait() { std::unique_lock<std::mutex> end(mutex); }
+  void event() { mutex.unlock(); }
+};
+} // namespace
+
+TEST_F(GoogleConnection, Instance) { EXPECT_TRUE(instance); }
+
+TEST_F(GoogleConnection, AddDataChannel) {
+  wait_for_event waiter;
+  instance->on_negotiation_needed.connect([&] { waiter.event(); });
+  instance->create_data_channel();
+  waiter.wait();
+}
+
+TEST_F(GoogleConnection, Offer) {
+  boost::future<::rtc::session_description> offer;
+  instance->on_negotiation_needed.connect(
+      [&] { offer = instance->create_offer(); });
+  instance->create_data_channel();
+  const ::rtc::session_description description = offer.get();
+  instance->close();
+  EXPECT_FALSE(description.sdp.empty());
+  EXPECT_EQ(description.type_, ::rtc::session_description::type::offer);
 }
 
---
+++ webrtc_peer/rtc/google/data_channel.hpp
@@ -0,0 +1,25 @@
+#ifndef RTC_GOOGLE_DATA_CHANNEL_HPP
+#define RTC_GOOGLE_DATA_CHANNEL_HPP
+
+#include "logging/logger.hpp"
+#include <api/data_channel_interface.h>
+#include <boost/signals2/signal.hpp>
+
+namespace rtc::google {
+class data_channel : protected ::webrtc::DataChannelObserver {
+public:
+  data_channel(::rtc::scoped_refptr<webrtc::DataChannelInterface> native_);
+  ~data_channel() override;
+
+protected:
+  void OnStateChange() override;
+  void OnMessage(const webrtc::DataBuffer &buffer) override;
+  void OnBufferedAmountChange(uint64_t sent_data_size) override;
+
+private:
+  logging::logger logger;
+  ::rtc::scoped_refptr<webrtc::DataChannelInterface> native;
+};
+} // namespace rtc::google
+
+#endif
