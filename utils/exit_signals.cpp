9d541d68d2ca3882cefcfd73e2fe159945ab7348
Markus Lanner <contact@markus-lanner.com>
2019-11-26 15:46:04 +0100

    added program options parsing

--- utils/exit_signals.cpp
+++ utils/exit_signals.cpp
@@ -15,7 +15,7 @@ exit_signals::~exit_signals() = default;
 
 void exit_signals::async_wait(callback_type callback) {
   impl->waiter.async_wait([this, callback](const auto &error, auto signal) {
-    BOOST_LOG_SEV(logger, logging::severity::info)
+    BOOST_LOG_SEV(logger, logging::severity::trace)
         << fmt::format("the signal '{}' occured, with the error:'{}'", signal,
                        error.message());
     callback(error);
