cedd94f93be5bdc7adc24a6bc9bbeab1846ead53
Markus Lanner <contact@markus-lanner.com>
2019-12-14 19:27:24 +0100

    capture device classes written

--- webrtc_peer/rtc/google/video_track_source.hpp
+++ webrtc_peer/rtc/google/video_track_source.hpp
@@ -1,12 +1,15 @@
 #ifndef RTC_GOOGLE_VIDEO_TRACK_ADAPTER_HPP
 #define RTC_GOOGLE_VIDEO_TRACK_ADAPTER_HPP
 
+#include "rtc/google/capture/video/device.hpp"
 #include <media/base/adapted_video_track_source.h>
 
 namespace rtc::google {
 class video_track_source {
 public:
   video_track_source();
+  ~video_track_source();
+  void handle_frame(const webrtc::VideoFrame &frame);
 
   class adapter : public rtc::AdaptedVideoTrackSource {
   public:
@@ -14,11 +17,13 @@ public:
     bool remote() const override;
     bool is_screencast() const override;
     absl::optional<bool> needs_denoising() const override;
+    void handle_frame(const webrtc::VideoFrame &frame);
   };
   rtc::scoped_refptr<adapter> source_adapter() const;
 
 protected:
-  rtc::scoped_refptr<adapter> adapter_;
+  const rtc::scoped_refptr<adapter> adapter_;
+  const std::shared_ptr<capture::video::device> source_device;
 };
 } // namespace rtc::google
 
