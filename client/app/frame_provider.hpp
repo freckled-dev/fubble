2b6b0033c1d6cef39d8728cd301563e55aaeb959
Markus Lanner <contact@markus-lanner.com>
2019-12-19 21:25:40 +0100

    camera is getting shown

---
+++ client/app/frame_provider.hpp
@@ -0,0 +1,53 @@
+#ifndef CLIENT_APP_FRAME_PROVIDER_HPP
+#define CLIENT_APP_FRAME_PROVIDER_HPP
+
+#include "logging/logger.hpp"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#include <QtMultimedia/qabstractvideosurface.h>
+#include <QtMultimedia/qvideosurfaceformat.h>
+#pragma GCC diagnostic pop
+
+class frame_provider : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(QAbstractVideoSurface *videoSurface READ videoSurface WRITE
+                 setVideoSurface)
+
+public:
+  QAbstractVideoSurface *videoSurface() const { return m_surface; }
+
+  void setVideoSurface(QAbstractVideoSurface *surface) {
+    BOOST_LOG_SEV(logger, logging::severity::debug)
+        << "setVideoSurface, surface:" << surface;
+    if (m_surface != surface && m_surface && m_surface->isActive()) {
+      m_surface->stop();
+    }
+    m_surface = surface;
+    if (m_surface)
+      m_surface->start(m_format);
+  }
+  void setFormat(int width, int heigth, QVideoFrame::PixelFormat pixel_format) {
+    QSize size(width, heigth);
+    QVideoSurfaceFormat format(size, pixel_format);
+    m_format = format;
+
+    if (!m_surface)
+      return;
+    if (m_surface->isActive())
+      m_surface->stop();
+    // m_format = m_surface->nearestFormat(m_format);
+    m_surface->start(m_format);
+  }
+public slots:
+  void onNewVideoContentReceived(const QVideoFrame &frame) {
+    if (m_surface)
+      m_surface->present(frame);
+  }
+
+private:
+  logging::logger logger;
+  QAbstractVideoSurface *m_surface{};
+  QVideoSurfaceFormat m_format;
+};
+
+#endif
--- client/app/main.qml
+++ client/app/main.qml
@@ -1,5 +1,7 @@
 import QtQuick 2.12
 import QtQuick.Window 2.12
+import QtMultimedia 5.0
+import io.fubble.FrameProvider 1.0
 
 Window {
   visible: true
@@ -7,9 +9,23 @@ Window {
   height: 480
   title: qsTr("Hello World")
 
+  FrameProvider {
+    objectName: "provider"
+    id: provider
+  }
+
+  VideoOutput {
+    id: display
+    objectName: "display"
+    anchors.fill: parent
+    source: provider
+  }
+
+  /*
   ClientForm {
     anchors.fill: parent
   }
+  */
 }
 
 
