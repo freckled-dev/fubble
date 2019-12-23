a1d071c94eb73fd10018486b3ee229dc91123eff
Markus Lanner <contact@markus-lanner.com>
2019-12-23 12:02:16 +0100

    peer_creator added

--- client/client/peer.cpp
+++ client/client/peer.cpp
@@ -2,7 +2,10 @@
 
 using namespace client;
 
-peer::peer(std::unique_ptr<signalling::client::client> signalling_,
+peer::peer(boost::executor &executor,
+           std::unique_ptr<signalling::client::client> signalling_,
            std::unique_ptr<rtc::connection> rtc_)
     : signalling_client(std::move(signalling_)),
-      rtc_connection(std::move(rtc_)) {}
+      rtc_connection(std::move(rtc_)),
+      ice_candidate_handler(*signalling_client, *rtc_connection),
+      offer_answer_handler(executor, *signalling_client, *rtc_connection) {}
--- client/client/peer.hpp
+++ client/client/peer.hpp
@@ -1,6 +1,8 @@
 #ifndef CLIENT_PEER_HPP
 #define CLIENT_PEER_HPP
 
+#include "p2p/negotiation/ice_candidates.hpp"
+#include "p2p/negotiation/offer_answer.hpp"
 #include "rtc/connection.hpp"
 #include "signalling/client/client.hpp"
 #include <memory>
@@ -9,12 +11,15 @@
 namespace client {
 class peer {
 public:
-  peer(std::unique_ptr<signalling::client::client> signalling_,
+  peer(boost::executor &executor,
+       std::unique_ptr<signalling::client::client> signalling_,
        std::unique_ptr<rtc::connection> rtc_);
 
 protected:
   std::unique_ptr<signalling::client::client> signalling_client;
   std::unique_ptr<rtc::connection> rtc_connection;
+  client::p2p::negotiation::ice_candidates ice_candidate_handler;
+  client::p2p::negotiation::offer_answer offer_answer_handler;
 };
 } // namespace client
 
