3f6c963a2956d4b39042bb503ce73abf0a3d1dfd
Markus Lanner <contact@markus-lanner.com>
2019-12-23 12:21:03 +0100

    added peers collection class

---
+++ client/client/peers.hpp
@@ -0,0 +1,23 @@
+#ifndef UUID_F6B4996F_8793_4480_BBA6_A14657BA6797
+#define UUID_F6B4996F_8793_4480_BBA6_A14657BA6797
+
+#include <memory>
+#include <vector>
+
+namespace client {
+class peer;
+class peers {
+public:
+  using peer_ptr = std::shared_ptr<peer>;
+
+  ~peers();
+  void add(const peer_ptr &add_);
+  std::vector<peer_ptr> get_all();
+  void remove(const peer_ptr &remove_);
+
+protected:
+  std::vector<peer_ptr> container;
+};
+} // namespace client
+
+#endif
