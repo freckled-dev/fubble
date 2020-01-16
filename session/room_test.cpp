07793df3d8224be0a81c0487bec3e011736f657d
Markus Lanner <contact@markus-lanner.com>
2020-01-16 23:54:30 +0100

    joins signals tested

--- session/room_test.cpp
+++ session/room_test.cpp
@@ -58,16 +58,73 @@ TEST_F(Room, Joiner) {
 }
 
 namespace {
-struct {};
+struct participants_waiter {
+  session::room &room;
+  int wait_until{2};
+  boost::promise<void> promise;
+  boost::signals2::scoped_connection joins;
+
+  participants_waiter(session::room &room) : room(room) {}
+  boost::future<void> operator()() {
+    joins = room.on_joins.connect([this](auto) { check_if_enough(); });
+    check_if_enough();
+    return promise.get_future();
+  }
+  void check_if_enough() {
+    if (static_cast<int>(room.get_participants().size()) != wait_until)
+      return;
+    joins.disconnect();
+    promise.set_value();
+  }
+};
 } // namespace
 
 TEST_F(Room, TwoJoins) {
   const auto room_id = uuid::generate();
   joined_client first{asio_executor, room_id};
   joined_client second{asio_executor, room_id};
-  auto first_joined = first();
-  auto second_joined = second();
-  boost::when_all(std::move(first_joined), std::move(second_joined))
-      .then(executor, [&](auto) { io_context.stop(); });
+  boost::when_all(first(), second()).then(executor, [&](auto rooms_future) {
+    // rooms_future is a future of a tuple of 2 futures
+    // https://www.boost.org/doc/libs/1_72_0/doc/html/thread/synchronization.html#thread.synchronization.futures.reference.when_all
+    auto rooms = rooms_future.get();
+    session::room_joiner::room_ptr first_room = std::get<0>(rooms).get();
+    session::room_joiner::room_ptr second_room = std::get<1>(rooms).get();
+    EXPECT_TRUE(first_room);
+    EXPECT_TRUE(second_room);
+    io_context.stop();
+  });
   io_context.run();
 }
+
+TEST_F(Room, TwoJoinSignals) {
+  const auto room_id = uuid::generate();
+  joined_client first{asio_executor, room_id};
+  joined_client second{asio_executor, room_id};
+  std::unique_ptr<session::room> first_room;
+  std::unique_ptr<session::room> second_room;
+  boost::when_all(first(), second()).then(executor, [&](auto rooms_future) {
+    auto rooms = rooms_future.get();
+    first_room = std::get<0>(rooms).get();
+    second_room = std::get<1>(rooms).get();
+    io_context.stop();
+  });
+  io_context.run();
+  io_context.restart();
+  ASSERT_TRUE(first_room);
+  ASSERT_TRUE(second_room);
+  participants_waiter first_waiter{*first_room};
+  participants_waiter second_waiter{*second_room};
+  boost::when_all(first_waiter(), second_waiter()).then(executor, [&](auto) {
+    io_context.stop();
+  });
+  io_context.run();
+}
+
+TEST_F(Room, Name) {
+  // TODO
+}
+
+TEST_F(Room, UpdateSignal) {
+  // TODO
+}
+
