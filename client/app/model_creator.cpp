1b36d5cc4c350799aa29e059db65a0f63261312f
Markus Lanner <contact@markus-lanner.com>
2020-02-07 10:47:51 +0100

    asio now run on ui thread

--- client/app/model_creator.cpp
+++ client/app/model_creator.cpp
@@ -3,12 +3,9 @@
 
 using namespace client;
 
-model_creator::model_creator(boost::executor &backend_thread)
-    : backend_thread(backend_thread) {}
+model_creator::model_creator() = default;
 
-  room_model *
-  model_creator::create_room_model(const std::shared_ptr<room> &room_,
-                                   QObject *parent) {
-    return new room_model(backend_thread, room_, parent);
-  }
-
+room_model *model_creator::create_room_model(const std::shared_ptr<room> &room_,
+                                             QObject *parent) {
+  return new room_model(room_, parent);
+}
--- client/app/model_creator.hpp
+++ client/app/model_creator.hpp
@@ -2,19 +2,18 @@
 #define UUID_7C6181D8_1BDC_4398_AC8C_618BA9F2CC5C
 
 #include <QObject>
-#include <boost/thread/executor.hpp>
+#include <memory>
 
 namespace client {
 class room_model;
 class room;
 class model_creator {
 public:
-  model_creator(boost::executor &backend_thread);
+  model_creator();
   room_model *create_room_model(const std::shared_ptr<room> &room_,
                                 QObject *parent);
 
 private:
-  boost::executor &backend_thread;
 };
 } // namespace client
 
