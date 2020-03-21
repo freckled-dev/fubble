353a662016b7dad4f221fddd4844fe56c900952f
Markus Lanner <contact@markus-lanner.com>
2020-03-21 06:14:41 +0100

    finished implemenation of temporary rooms

--- temporary_room/rooms/rooms_test.cpp
+++ temporary_room/rooms/rooms_test.cpp
@@ -13,6 +13,7 @@ public:
 class mock_participant : public participant {
 public:
   MOCK_METHOD(void, set_room, (const room_id &), (override));
+  MOCK_METHOD(void, set_error, (const std::runtime_error &), (override));
 };
 
 TEST(Rooms, Instance) {
@@ -30,6 +31,79 @@ TEST(Rooms, Add) {
   EXPECT_CALL(participant_, set_room(room_id_));
   rooms test{room_factory_};
   test.add_participant(participant_, "name");
+  EXPECT_EQ(test.get_room_count(), 1);
 }
 
-// TODO test for failed room creation
+TEST(Rooms, AddTwoParticipants) {
+  mock_room_factory room_factory_;
+  room_id room_id_{"fun_id"};
+  boost::promise<room_id> create_promise;
+  EXPECT_CALL(room_factory_, create)
+      .WillOnce(
+          ::testing::Return(::testing::ByMove(create_promise.get_future())));
+  std::array<mock_participant, 2> participants;
+  rooms test{room_factory_};
+  for (auto &participant_ : participants) {
+    EXPECT_CALL(participant_, set_room(room_id_));
+    test.add_participant(participant_, "name");
+  }
+  EXPECT_EQ(test.get_room_count(), 1);
+  create_promise.set_value(room_id_);
+  EXPECT_EQ(test.get_room_count(), 1);
+}
+
+MATCHER_P(HasCorrectError, message, "") { return arg.what() == message; }
+
+TEST(Rooms, AddFail) {
+  mock_room_factory room_factory_;
+  room_id room_id_{"fun_id"};
+  std::string error_message = "failed";
+  EXPECT_CALL(room_factory_, create)
+      .WillOnce(::testing::Return(
+          ::testing::ByMove(boost::make_exceptional_future<room_id>(
+              std::runtime_error(error_message)))));
+  mock_participant participant_;
+  EXPECT_CALL(participant_, set_error(HasCorrectError(error_message)));
+  rooms test{room_factory_};
+  test.add_participant(participant_, "name");
+}
+
+struct RoomWithParticipants : testing::Test {
+  mock_room_factory room_factory_;
+  room_id room_id_{"fun_id"};
+  room_name room_name_{"room_name"};
+  rooms test{room_factory_};
+  std::vector<std::shared_ptr<mock_participant>> participants;
+
+  RoomWithParticipants() {
+    EXPECT_CALL(room_factory_, create)
+        .WillOnce(::testing::Return(
+            ::testing::ByMove(boost::make_ready_future<room_id>(room_id_))));
+  }
+  void add_participants(int count) {
+    for (int index = 0; index < count; ++index) {
+      auto participant_ = std::make_shared<mock_participant>();
+      participants.push_back(participant_);
+      EXPECT_CALL(*participant_, set_room(room_id_));
+      test.add_participant(*participant_, room_name_);
+    }
+  }
+  void remove_all_participants() {
+    for (auto &participant_ : participants)
+      test.remove_participant(*participant_, room_name_);
+  }
+};
+
+TEST_F(RoomWithParticipants, Remove) {
+  add_participants(1);
+  EXPECT_CALL(room_factory_, destroy(room_id_));
+  remove_all_participants();
+  EXPECT_EQ(test.get_room_count(), 0);
+}
+
+TEST_F(RoomWithParticipants, RemoveTwo) {
+  add_participants(2);
+  EXPECT_CALL(room_factory_, destroy(room_id_));
+  remove_all_participants();
+  EXPECT_EQ(test.get_room_count(), 0);
+}
