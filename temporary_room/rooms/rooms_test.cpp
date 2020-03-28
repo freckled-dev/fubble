39547bb122bbc573da79ad044735094ab4f74300
Markus Lanner <contact@markus-lanner.com>
2020-03-28 13:16:44 +0100

    refactored rooms::rooms to better fit matrix

--- temporary_room/rooms/rooms_test.cpp
+++ temporary_room/rooms/rooms_test.cpp
@@ -6,14 +6,13 @@ using namespace temporary_room::rooms;
 
 class mock_room_factory : public room_factory {
 public:
-  MOCK_METHOD(boost::future<room_id>, create, (), (override));
-  MOCK_METHOD(void, destroy, (const room_id &), (override));
+  MOCK_METHOD(boost::future<room_ptr>, create, (), (override));
 };
 
-class mock_participant : public participant {
+class mock_room : public room {
 public:
-  MOCK_METHOD(void, set_room, (const room_id &), (override));
-  MOCK_METHOD(void, set_error, (const std::runtime_error &), (override));
+  std::shared_ptr<int> alive_check = std::make_shared<int>(42);
+  MOCK_METHOD(room_id, get_room_id, (), (const override));
 };
 
 TEST(Rooms, Instance) {
@@ -23,32 +22,38 @@ TEST(Rooms, Instance) {
 
 TEST(Rooms, Add) {
   mock_room_factory room_factory_;
-  room_id room_id_{"fun_id"};
+  const room_id room_id_{"fun_id"};
+  auto room_ = std::make_unique<mock_room>();
+  EXPECT_CALL(*room_, get_room_id).WillOnce(::testing::Return(room_id_));
+  room_ptr room_casted = std::move(room_);
   EXPECT_CALL(room_factory_, create)
       .WillOnce(::testing::Return(
-          ::testing::ByMove(boost::make_ready_future<room_id>(room_id_))));
-  mock_participant participant_;
-  EXPECT_CALL(participant_, set_room(room_id_));
+          ::testing::ByMove(boost::make_ready_future(std::move(room_casted)))));
   rooms test{room_factory_};
-  test.add_participant(participant_, "name");
+  auto add_future = test.get_or_create_room_id("name");
+  EXPECT_EQ(add_future.get(), room_id_);
   EXPECT_EQ(test.get_room_count(), 1);
 }
 
 TEST(Rooms, AddTwoParticipants) {
   mock_room_factory room_factory_;
   room_id room_id_{"fun_id"};
-  boost::promise<room_id> create_promise;
+  boost::promise<room_ptr> create_promise;
   EXPECT_CALL(room_factory_, create)
       .WillOnce(
           ::testing::Return(::testing::ByMove(create_promise.get_future())));
-  std::array<mock_participant, 2> participants;
   rooms test{room_factory_};
-  for (auto &participant_ : participants) {
-    EXPECT_CALL(participant_, set_room(room_id_));
-    test.add_participant(participant_, "name");
+  int called{};
+  for (int counter{}; counter < 2; ++counter) {
+    test.get_or_create_room_id("name").then([&](auto result) {
+      result.get();
+      ++called;
+    });
   }
   EXPECT_EQ(test.get_room_count(), 1);
-  create_promise.set_value(room_id_);
+  auto room_ = std::make_unique<mock_room>();
+  EXPECT_CALL(*room_, get_room_id).WillOnce(::testing::Return(room_id_));
+  create_promise.set_value(std::move(room_));
   EXPECT_EQ(test.get_room_count(), 1);
 }
 
@@ -56,16 +61,14 @@ MATCHER_P(HasCorrectError, message, "") { return arg.what() == message; }
 
 TEST(Rooms, AddFail) {
   mock_room_factory room_factory_;
-  room_id room_id_{"fun_id"};
   std::string error_message = "failed";
   EXPECT_CALL(room_factory_, create)
       .WillOnce(::testing::Return(
-          ::testing::ByMove(boost::make_exceptional_future<room_id>(
+          ::testing::ByMove(boost::make_exceptional_future<room_ptr>(
               std::runtime_error(error_message)))));
-  mock_participant participant_;
-  EXPECT_CALL(participant_, set_error(HasCorrectError(error_message)));
   rooms test{room_factory_};
-  test.add_participant(participant_, "name");
+  auto result = test.get_or_create_room_id("name");
+  EXPECT_THROW(result.get(), std::runtime_error);
 }
 
 struct RoomWithParticipants : testing::Test {
@@ -73,37 +76,46 @@ struct RoomWithParticipants : testing::Test {
   room_id room_id_{"fun_id"};
   room_name room_name_{"room_name"};
   rooms test{room_factory_};
-  std::vector<std::shared_ptr<mock_participant>> participants;
+  std::vector<boost::future<room_id>> participants;
+  room *room_;
+  std::shared_ptr<int> room_alive_check;
 
   RoomWithParticipants() {
+    auto room_ = std::make_unique<mock_room>();
+    this->room_ = room_.get();
+    room_alive_check = room_->alive_check;
+    EXPECT_CALL(*room_, get_room_id)
+        .WillRepeatedly(::testing::Return(room_id_));
+    room_ptr room_casted = std::move(room_);
     EXPECT_CALL(room_factory_, create)
-        .WillOnce(::testing::Return(
-            ::testing::ByMove(boost::make_ready_future<room_id>(room_id_))));
+        .WillOnce(::testing::Return(::testing::ByMove(
+            boost::make_ready_future(std::move(room_casted)))));
   }
   void add_participants(int count) {
-    for (int index = 0; index < count; ++index) {
-      auto participant_ = std::make_shared<mock_participant>();
-      participants.push_back(participant_);
-      EXPECT_CALL(*participant_, set_room(room_id_));
-      test.add_participant(*participant_, room_name_);
-    }
+    for (int index = 0; index < count; ++index)
+      participants.emplace_back(test.get_or_create_room_id(room_name_));
   }
-  void remove_all_participants() {
+
+  void remove_all_participants() { room_->on_participant_count_changed(1); }
+
+  void ensure_room_id_got_set() {
     for (auto &participant_ : participants)
-      test.remove_participant(*participant_, room_name_);
+      EXPECT_EQ(participant_.get(), room_id_);
   }
 };
 
 TEST_F(RoomWithParticipants, Remove) {
   add_participants(1);
-  EXPECT_CALL(room_factory_, destroy(room_id_));
+  ensure_room_id_got_set();
   remove_all_participants();
+  EXPECT_TRUE(room_alive_check.unique());
   EXPECT_EQ(test.get_room_count(), 0);
 }
 
 TEST_F(RoomWithParticipants, RemoveTwo) {
   add_participants(2);
-  EXPECT_CALL(room_factory_, destroy(room_id_));
+  ensure_room_id_got_set();
   remove_all_participants();
+  EXPECT_TRUE(room_alive_check.unique());
   EXPECT_EQ(test.get_room_count(), 0);
 }
---
+++ temporary_room/server/server.hpp
@@ -0,0 +1,20 @@
+#ifndef UUID_73DE619B_B268_48C3_BDB5_5A6C948A49E6
+#define UUID_73DE619B_B268_48C3_BDB5_5A6C948A49E6
+
+#include "temporary_room/net/server.hpp"
+#include "temporary_room/rooms/rooms.hpp"
+
+namespace temporary_room::server {
+class server {
+public:
+  server(net::server::server &net_server, rooms::rooms &rooms);
+
+protected:
+  boost::future<std::string> on_join(const std::string &name);
+
+  net::server::server &net_server;
+  rooms::rooms &rooms;
+};
+} // namespace temporary_room::server
+
+#endif
