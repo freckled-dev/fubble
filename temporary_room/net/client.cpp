1c605e23e41dbd241da9644a605091f41e63f6e5
Markus Lanner <contact@markus-lanner.com>
2020-03-28 09:13:51 +0100

    added temporary_room::server and test

---
+++ temporary_room/net/client.cpp
@@ -0,0 +1,28 @@
+#include "client.hpp"
+#include "http/client.hpp"
+#include <boost/thread/future.hpp>
+#include <fmt/format.h>
+
+using namespace temporary_room::net;
+
+error::error(boost::beast::http::status code, const nlohmann::json &error_)
+    : message(fmt::format("code:'{}', error_json:{}", code, error_.dump())) {}
+
+const char *error::what() const noexcept { return message.c_str(); }
+
+client::client(http::client &http_client) : http_client(http_client) {}
+
+boost::future<std::string> client::join(const std::string &name) {
+  auto content = nlohmann::json::object();
+  content["name"] = name;
+  auto target = fmt::format("join/{}", name);
+  return http_client.put(target, content).then(executor, [](auto result) {
+    auto got_result = result.get();
+    if (got_result.first != boost::beast::http::status::ok)
+      throw error(got_result.first, got_result.second);
+    auto response = got_result.second;
+    std::string room_id = response["room_id"];
+    return room_id;
+  });
+}
+
--- temporary_room/net/server.cpp
+++ temporary_room/net/server.cpp
@@ -1,136 +1,25 @@
 #include "server.hpp"
-#include <boost/beast/version.hpp>
 
 using namespace temporary_room::net::server;
 
-connection::connection(boost::asio::ip::tcp::socket &&socket)
-    : stream{std::move(socket)} {}
-
-boost::future<void> connection::do_() {
-  read_next_request();
-  return promise->get_future();
-}
-
-bool connection::check_error(const boost::system::error_code error) {
-  if (!error)
-    return false;
-  auto promise_copy = promise;
-  if (error == boost::beast::http::error::end_of_stream) {
-    BOOST_LOG_SEV(logger, logging::severity::trace) << "end_of_stream";
-    promise_copy->set_value();
-  } else
-    promise_copy->set_exception(boost::system::system_error(error));
-  return true;
-}
-
-void connection::read_next_request() {
-  BOOST_LOG_SEV(logger, logging::severity::trace) << "read_next_request";
-  request = std::make_shared<request_type>();
-  boost::beast::http::async_read(stream, buffer, *request,
-                                 [this](auto error, auto) {
-                                   if (check_error(error))
-                                     return;
-                                   on_got_request(*request);
-                                 });
-}
-
-void connection::on_got_request(const request_type &request) {
-  const std::string request_body = request.body();
-  nlohmann::json content = nlohmann::json::parse(request_body);
-  const auto target = request.target().to_string();
-  auto response = on_request(target, content);
-  response.then([this](auto result) { on_got_response(result); });
-}
-
-void connection::on_got_response(response_future &response_future_) {
-  response = std::make_shared<response_type>(boost::beast::http::status::ok,
-                                             request->version());
-  response->set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
-  response->set(boost::beast::http::field::content_type, "application/json");
-  response->keep_alive(request->keep_alive());
-  try {
-    auto response_content = response_future_.get().dump();
-    response->body() = response_content;
-  } catch (const std::exception &error) {
-    BOOST_LOG_SEV(logger, logging::severity::warning)
-        << "an error occured while getting the response, error:"
-        << error.what();
-    response->result(boost::beast::http::status::internal_server_error);
-    nlohmann::json response_content = nlohmann::json::object();
-    response_content["description"] = error.what();
-    response->body() = response_content.dump();
-  }
-  response->prepare_payload();
-  send_response();
-}
-
-void connection::send_response() {
-  BOOST_LOG_SEV(logger, logging::severity::trace) << "send_response";
-  boost::beast::http::async_write(stream, *response, [this](auto error, auto) {
-    if (check_error(error))
-      return;
-    if (response->need_eof())
-      stream.socket().shutdown(boost::asio::socket_base::shutdown_both);
-    read_next_request();
-  });
-}
-
-acceptor::acceptor(boost::asio::io_context &context, const config &config_)
-    : context(context), config_(config_) {}
-
-void acceptor::stop() {
-  BOOST_LOG_SEV(logger, logging::severity::info) << "server gets stopped";
-  acceptor_.close();
-}
-
-boost::future<void> acceptor::run() {
-  run_promise = std::make_shared<boost::promise<void>>();
-  const auto address = boost::asio::ip::make_address("0.0.0.0");
-  const boost::asio::ip::tcp::endpoint endpoint{address, config_.port};
-  acceptor_.open(endpoint.protocol());
-  acceptor_.set_option(boost::asio::socket_base::reuse_address(true));
-  acceptor_.listen();
-  accept_next();
-  return run_promise->get_future();
-}
-
-unsigned short acceptor::get_port() const {
-  return acceptor_.local_endpoint().port();
-}
-
-void acceptor::accept_next() {
-  BOOST_LOG_SEV(logger, logging::severity::trace) << "accept_next";
-  socket = std::make_unique<boost::asio::ip::tcp::socket>(context);
-  boost::system::error_code error;
-  acceptor_.async_accept(*socket, [this](auto error) {
-    if (error) {
-      auto promise_copy = run_promise;
-      if (error == boost::asio::error::operation_aborted) {
-        BOOST_LOG_SEV(logger, logging::severity::trace) << "operation_aborted";
-        run_promise->set_value(); // stop() got called
-      } else
-        run_promise->set_exception(boost::system::system_error(error));
-      return;
-    }
-    do_session();
-    accept_next();
-  });
-}
-
-void acceptor::do_session() {
-  auto connection_ = std::make_shared<connection>(std::move(*socket));
-  socket.reset();
-  connection_->on_request = [this](const auto &target, const auto &content) {
-    return on_request(target, content);
+server::server(acceptor &net_server) : acceptor_(net_server) {
+  acceptor_.on_request =
+      [this](const std::string &target,
+             const nlohmann::json &request) -> response_future {
+    (void)request;
+    return on_request(target);
   };
-  connection_->do_().then(executor, [this, connection_](auto result) {
-    // safed the reference of connection in lambda capture.
-    try {
-      result.get();
-    } catch (const std::exception &error) {
-      BOOST_LOG_SEV(logger, logging::severity::warning)
-          << "an error occured while doing the connection, error:"
-          << error.what();
-    }
+}
+
+response_future server::on_request(const std::string &target) {
+  const std::string prefix = "join/";
+  if (target.rfind(prefix, 0) != 0)
+    return boost::make_exceptional_future<nlohmann::json>(
+        std::runtime_error("invalid target"));
+  const auto name = target.substr(target.find("/") + 1);
+  return on_join(name).then([](auto result) {
+    auto response = nlohmann::json::object();
+    response["room_id"] = result.get();
+    return response;
   });
 }
