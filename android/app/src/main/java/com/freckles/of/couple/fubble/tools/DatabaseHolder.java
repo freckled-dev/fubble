9971fe47bd1144aea86de12ae7b8be8c4a14fbdf
Manu Weilharter <manuela.weilharter@gmail.com>
2020-04-01 09:45:14 +0200

    removed old projects

--- android/app/src/main/java/com/freckles/of/couple/fubble/tools/DatabaseHolder.java
+++
@@ -1,9 +0,0 @@
-package com.freckles.of.couple.fubble.tools;
-
-public class DatabaseHolder {
-
-    public static final String FUBBLE_DATABASE_NAME = "fubble_db";
-
-    public static AppDatabase database;
-
-}
