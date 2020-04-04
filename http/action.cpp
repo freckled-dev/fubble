6835af3ac897201a0ae4f2eba49772bd9aa7f0e2
Markus Lanner <contact@markus-lanner.com>
2020-04-04 19:32:13 +0200

    client room_test work again

--- http/action.cpp
+++ http/action.cpp
@@ -12,6 +12,7 @@ action::action(boost::asio::io_context &context, boost::beast::http::verb verb,
   // this class
   promise = std::make_shared<async_result_promise>();
   std::string target_with_prefix = fields_.target_prefix + target;
+  auto &request = buffers_->request;
   request = request_type{verb, target_with_prefix, fields_.version};
   request.set(boost::beast::http::field::host, fields_.host);
   request.set(boost::beast::http::field::user_agent, fields_.agent);
@@ -28,26 +29,27 @@ action::~action() {
       "action is getting destructed before done, target:{}", target);
   promise->set_exception(
       boost::system::system_error(boost::asio::error::operation_aborted));
-  promise.reset();
-  cancel();
+  stream.cancel();
 }
 
 void action::set_request_body(const nlohmann::json &body) {
+  auto &request = buffers_->request;
   request.body() = body.dump();
 }
 
 action::async_result_future action::do_() {
+  auto &request = buffers_->request;
   request.prepare_payload();
   BOOST_LOG_SEV(logger, logging::severity::trace) << fmt::format(
       "resolving, server:'{}', port:'{}'", server_.server, server_.port);
   std::weak_ptr<int> alive = alive_check;
-  resolver.async_resolve(
-      server_.server, server_.port,
-      [this, alive = std::move(alive)](auto error, auto result) {
-        if (!alive.lock())
-          return;
-        on_resolved(error, result);
-      });
+  resolver.async_resolve(server_.server, server_.port,
+                         [buffers_ = buffers_, this,
+                          alive = std::move(alive)](auto error, auto result) {
+                           if (!alive.lock())
+                             return;
+                           on_resolved(error, result);
+                         });
   return promise->get_future();
 }
 
@@ -60,12 +62,12 @@ void action::on_resolved(
   if (!check_and_handle_error(error))
     return;
   std::weak_ptr<int> alive = alive_check;
-  stream.async_connect(resolved,
-                       [this, alive = std::move(alive)](auto error, auto) {
-                         if (!alive.lock())
-                           return;
-                         on_connected(error);
-                       });
+  stream.async_connect(resolved, [buffers_ = buffers_, this,
+                                  alive = std::move(alive)](auto error, auto) {
+    if (!alive.lock())
+      return;
+    on_connected(error);
+  });
 }
 
 void action::on_connected(const boost::system::error_code &error) {
@@ -74,7 +76,8 @@ void action::on_connected(const boost::system::error_code &error) {
     return;
   std::weak_ptr<int> alive = alive_check;
   boost::beast::http::async_write(
-      stream, request, [this, alive = std::move(alive)](auto error, auto) {
+      stream, buffers_->request,
+      [buffers_ = buffers_, this, alive = std::move(alive)](auto error, auto) {
         if (!alive.lock())
           return;
         on_request_send(error);
@@ -90,8 +93,8 @@ void action::on_request_send(const boost::system::error_code &error) {
 void action::read_response() {
   std::weak_ptr<int> alive = alive_check;
   boost::beast::http::async_read(
-      stream, response_buffer, response,
-      [this, alive = std::move(alive)](auto error, auto) {
+      stream, buffers_->response_buffer, buffers_->response,
+      [buffers_ = buffers_, this, alive = std::move(alive)](auto error, auto) {
         if (!alive.lock())
           return;
         on_response_read(error);
@@ -101,6 +104,7 @@ void action::read_response() {
 void action::on_response_read(const boost::system::error_code &error) {
   if (!check_and_handle_error(error))
     return;
+  auto &response = buffers_->response;
   auto http_code = response.result();
   stream.socket().shutdown(boost::asio::socket_base::shutdown_both);
   auto promise_copy = std::move(promise);
