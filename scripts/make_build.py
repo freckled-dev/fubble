5aba75a00445a969d9617860017da39cf72b8d46
Markus Lanner <contact@markus-lanner.com>
2020-05-01 19:49:52 +0200

    added landing-page to deployment

--- scripts/make_build.py
+++ scripts/make_build.py
@@ -3,30 +3,7 @@
 import os
 import subprocess
 import shutil
-
-print('make_build')
-
-def get_first_line_of_subprocess_result(result):
-    return result.stdout.decode('utf-8').partition('\n')[0]
-
-def get_environment_variable_or(variable, or_):
-    result = os.environ.get(variable)
-    if result:
-        return result
-    return or_
-
-class Paths:
-    def __init__(self):
-        script_dir = os.path.realpath(__file__)
-        git_result = subprocess.run(['git', 'rev-parse', '--show-toplevel'],
-            stdout=subprocess.PIPE, check=True)
-        self.source_dir = get_first_line_of_subprocess_result(git_result)
-        self.build_dir = os.path.join(self.source_dir, '..', 'fubble_build')
-        self.build_dir = get_environment_variable_or('FUBBLE_BUILD_DIR', self.build_dir)
-        self.meson_dir = os.path.join(self.build_dir, 'meson')
-        self.dependencies_dir = os.path.join(self.build_dir, 'dependencies')
-        self.prefix_dir = os.path.join(self.build_dir, 'install')
-        self.prefix_dir = get_environment_variable_or('FUBBLE_PREFIX_DIR', self.prefix_dir)
+from paths import Paths
 
 paths = Paths()
 try:
