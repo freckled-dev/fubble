02927770942504680c5d1b1d393b12b4dc3fcb92
Markus Lanner <contact@markus-lanner.com>
2020-05-02 20:20:05 +0200

    fixing string for linux deploy path

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -11,11 +11,14 @@ paths = Paths()
 script_dir = pathlib.Path(__file__).parent.resolve()
 deploy_dir = os.path.join(script_dir, 'deploy')
 os.chdir(script_dir)
+
+
+install_linux = os.path.join(script_dir, '..', 'install')
 install_windows = os.path.join(script_dir, '..', 'install_windows')
 
 subprocess.run(['ansible-playbook', 
     '-i', 'deploy/inventory/production.yml',
-    '-e', 'fubble_binaries_dir=%s' % (paths.prefix_dir),
+    '-e', 'fubble_binaries_dir=%s' % (install_linux),
     '-e', 'fubble_binaries_windows_dir="%s"' % (install_windows),
     'deploy/site.yml'],
         check=True)
