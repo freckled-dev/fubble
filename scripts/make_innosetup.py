a3e84992219e6cd6b5a409264163363cee2aed79
Markus Lanner <contact@markus-lanner.com>
2020-05-07 00:42:49 -0700

    enabled artifacts. fixed innosetup

--- scripts/make_innosetup.py
+++ scripts/make_innosetup.py
@@ -19,10 +19,11 @@ shutil.copy2(inno_file, innosetup_build_dir)
 updater_bin_dir = os.path.join(paths.source_dir, 'updater', 'bin', 'Release')
 copy_tree(updater_bin_dir, innosetup_build_dir)
 
-os.chdir(updater_bin_dir)
+os.chdir(innosetup_build_dir)
+
 subprocess.run(['curl.exe', '-Lo', 'rclone.zip',
     'https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-windows-amd64.zip'], 
     check=True)
-subprocess.run(['7z.exe', 'e', 'rclone.zip'], check=True)
+subprocess.run(['7z.exe', 'e', '-r', 'rclone.zip', '*.exe'], check=True)
 
 subprocess.run(['iscc', '/O' + paths.source_dir, 'Fubble_Installer.iss'], check=True)
