755b72d20e7a9fb3108adbb52d1b7c4851b714ee
Markus Lanner <contact@markus-lanner.com>
2020-05-08 21:08:02 +0200

    added file logging to fubble executable

--- utils/logging/initialser.cpp
+++ utils/logging/initialser.cpp
@@ -6,6 +6,8 @@
 #include <boost/log/support/date_time.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <filesystem>
 #include <iostream>
 
 BOOST_LOG_ATTRIBUTE_KEYWORD(tag_attr, "Tag", std::string)
@@ -34,8 +36,7 @@ void colorized_console_formatter(boost::log::record_view const &rec,
 
   boost::log::value_ref<severity> severity_ =
       boost::log::extract<severity>("Severity", rec);
-  if (!severity_)
-    throw std::runtime_error("colorized_console_formatter: extraction failed");
+  BOOST_ASSERT(severity_);
   bool colored{};
   switch (severity_.get()) {
   // TODO evaluate https://github.com/agauniyal/rang for color setting
@@ -66,9 +67,29 @@ void colorized_console_formatter(boost::log::record_view const &rec,
   if (colored)
     strm << "\033[0m";
 }
-} // namespace
+void file_formatter(boost::log::record_view const &rec,
+                    boost::log::formatting_ostream &strm) {
+  using namespace logging;
+  namespace expr = boost::log::expressions;
+  namespace attrs = boost::log::attributes;
 
-void logging::add_console_log() {
+  boost::log::value_ref<severity> severity_ =
+      boost::log::extract<severity>("Severity", rec);
+  BOOST_ASSERT(severity_);
+  date_time_formatter(rec, strm) << " ";
+  process_formatter(rec, strm) << " ";
+  thread_formatter(rec, strm) << " ";
+  strm << severity_;
+  strm << " [";
+  module_formatter(rec, strm);
+  strm << "] [";
+  tag_formatter(rec, strm);
+  strm << "] " << rec[boost::log::expressions::smessage];
+}
+void initialise_globals() {
+  static bool done{};
+  if (done)
+    return;
   namespace expr = boost::log::expressions;
   namespace attrs = boost::log::attributes;
   boost::log::add_common_attributes();
@@ -76,7 +97,25 @@ void logging::add_console_log() {
   core->add_global_attribute("TimeStamp", attrs::utc_clock());
   core->add_global_attribute("PID", attrs::current_process_id());
   core->add_global_attribute("TID", attrs::current_thread_id());
+}
+} // namespace
+
+void logging::add_console_log() {
+  initialise_globals();
   boost::log::add_console_log(std::cout,
                               boost::log::keywords::auto_flush = true)
       ->set_formatter(&colorized_console_formatter);
+}
+
+void logging::add_file_log() {
+  initialise_globals();
+  std::error_code error;
+  auto temporary_dir = std::filesystem::temp_directory_path(error);
+  BOOST_ASSERT(!error);
+  if (error)
+    return;
+  auto log_file = temporary_dir / "fubble.log";
+  boost::log::add_file_log(log_file, boost::log::keywords::auto_flush = true,
+                           boost::log::keywords::open_mode = std::ios_base::app)
+      ->set_formatter(&file_formatter);
 }
