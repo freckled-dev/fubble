53c85ddcd4fbdae3af298eb2eb393a6b4bf4ff37
Markus Lanner <contact@markus-lanner.com>
2020-05-13 20:02:54 +0200

    ensured that only the latest leave/invite/join is set

--- matrix/room_participant.hpp
+++ matrix/room_participant.hpp
@@ -1,24 +1,30 @@
 #ifndef UUID_9A5F9DB4_A585_471D_9A8C_641DE3A54DF9
 #define UUID_9A5F9DB4_A585_471D_9A8C_641DE3A54DF9
 
+#include "logger.hpp"
 #include <boost/signals2/signal.hpp>
+#include <chrono>
 
 namespace matrix {
 class user;
 enum class join_state { join, leave, invite };
 class room_participant {
 public:
-  room_participant(user &user_, const join_state join_state_);
+  using timestamp_type = std::chrono::system_clock::time_point;
+  room_participant(user &user_, const join_state join_state_,
+                   const timestamp_type &join_state_timestamp);
 
   user &get_user() const;
   join_state get_join_state() const;
-  void set_join_state(const join_state new_);
+  void set_join_state(const join_state new_, const timestamp_type &timestamp);
   std::string get_id() const;
 
   boost::signals2::signal<void()> on_join_state_changed;
 
 protected:
   user &user_;
+  matrix::logger logger;
+  timestamp_type join_state_timestamp;
   join_state join_state_;
 };
 } // namespace matrix
