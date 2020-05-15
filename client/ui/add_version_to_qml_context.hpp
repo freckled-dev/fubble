35298cea68458d31c45f8a98726acf9b98f3bbbb
Markus Lanner <contact@markus-lanner.com>
2020-05-15 13:17:45 +0200

    versioning added to qml

---
+++ client/ui/add_version_to_qml_context.hpp
@@ -0,0 +1,16 @@
+#ifndef UUID_B7B594BF_DE0A_47F4_A904_957FE68C8BE5
+#define UUID_B7B594BF_DE0A_47F4_A904_957FE68C8BE5
+
+class QQmlContext;
+
+namespace client::ui {
+class add_version_to_qml_context {
+public:
+  add_version_to_qml_context(QQmlContext &context);
+
+protected:
+  // QString version;
+};
+} // namespace client::ui
+
+#endif
