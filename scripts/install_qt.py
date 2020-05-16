c4d5234a06b25212931be58e876fc420ca5f5616
Markus Lanner <contact@markus-lanner.com>
2020-05-16 09:47:03 +0200

    qt should be only installed if not already installed

---
+++ scripts/install_qt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import platform
+import shutil
+import pathlib
+from paths import Paths
+
+class UnsupportedPlatform(Exception):
+    pass
+
+# https://stackoverflow.com/questions/1854/what-os-am-i-running-on
+if platform.system() != "Windows":
+    raise UnsupportedPlatform()
+
+qt_version = "5.15.0"
+qt_prefix_dir = "C:/Qt"
+qt_platform = "desktop"
+qt_system = "windows"
+qt_target = "win64_msvs2019_64"
+
+final_dir = os.path.join(qt_prefix_dir, qt_version, qt_target)
+qmake_path = os.path.join(final_dir, 'bin', 'qmake')
+if os.path.exists(qmake_path):
+    print("skipping installation of qt, because '%s' does already exists" % qmake_path)
+    return
+
+subprocess.run(['aqt', 'install', 
+    '--outputdir', qt_prefix_dir,
+    qt_version, qt_system, qt_platform, qt_target],
+        check=True)
+
+
