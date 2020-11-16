12dcc3a4269b4820778af66d1f4fc618cb7fd892
Markus Lanner <contact@markus-lanner.com>
2020-11-16 06:12:24 -0800

    logging matrix client sync errors

--- matrix/client_synchronizer.cpp
+++ matrix/client_synchronizer.cpp
@@ -24,6 +24,8 @@ public:
     try {
       result.get();
     } catch (const boost::system::system_error &error) {
+      BOOST_LOG_SEV(logger, logging::severity::debug)
+            << __FUNCTION__ << ", sync got cancelled, stopping the sync, error:code():" << error.code() << ", what:" << error.what();
       if (error.code() == boost::asio::error::operation_aborted) {
         BOOST_LOG_SEV(logger, logging::severity::debug)
             << __FUNCTION__ << ", sync got cancelled, stopping the sync";
