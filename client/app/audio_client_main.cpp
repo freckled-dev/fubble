04793387874de3a104f7006bb976d172e338838f
Markus Lanner <contact@markus-lanner.com>
2020-11-23 20:43:22 +0100

    send-audio is now a program option

--- client/app/audio_client_main.cpp
+++ client/app/audio_client_main.cpp
@@ -3,14 +3,32 @@
 #include "logging/logger.hpp"
 #include "rtc/google/log_webrtc_to_logging.hpp"
 #include "utils/exit_signals.hpp"
+#include "utils/options_adder.hpp"
 #include <boost/asio/io_context.hpp>
+#include <iostream>
 
-int main(int, char *[]) {
+namespace bpo = boost::program_options;
+
+int main(int argc, char *argv[]) {
+  audio_client::audio_client::config audio_client_config;
+
+  bpo::options_description general("general");
+  general.add_options()("help", "produce help message");
+  utils::option_adder adder{general};
+  adder.add("send-audio", audio_client_config.send_audio,
+            "send your own audio stream");
+  bpo::variables_map vm;
+  bpo::store(bpo::parse_command_line(argc, argv, general), vm);
+  bpo::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << general << std::endl;
+    return 0;
+  }
+
   logging::add_console_log(logging::severity::debug);
   rtc::google::log_webrtc_to_logging webrtc_logger;
   webrtc_logger.set_enabled(false);
-  logging::logger logger{"main"};
-  audio_client::audio_client::config audio_client_config;
   auto audio_client = audio_client::audio_client::create(audio_client_config);
   boost::asio::executor exit_executor = audio_client->get_core()
                                             ->get_utils_executor_module()
