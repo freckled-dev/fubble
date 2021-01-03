2d648bfd6f7fe10512cb65f9f8d6fd033108cd30
Markus Lanner <contact@markus-lanner.com>
2021-01-03 16:52:12 +0100

    there shall be no boost header included when using logging/initialser part 2

---
+++ fubble/utils/logging/severity.hpp
@@ -0,0 +1,8 @@
+#ifndef UUID_F82A343B_D46A_49C5_8576_C60B65748821
+#define UUID_F82A343B_D46A_49C5_8576_C60B65748821
+
+namespace logging {
+enum severity { trace, debug, info, warning, error, fatal };
+}
+
+#endif
