d525137b96ee728746f960fdc01d0a5af5a5fd02
Markus Lanner <contact@markus-lanner.com>
2021-01-03 17:42:36 +0100

    exposing more api

--- fubble/rtc/google/log_webrtc_to_logging.hpp
+++ fubble/rtc/google/log_webrtc_to_logging.hpp
@@ -1,9 +1,11 @@
 #ifndef UUID_CF1BCC72_006A_4D84_9FAA_0C1588DD2338
 #define UUID_CF1BCC72_006A_4D84_9FAA_0C1588DD2338
 
+#include "fubble/utils/export.hpp"
+
 namespace rtc {
 namespace google {
-class log_webrtc_to_logging {
+class FUBBLE_PUBLIC log_webrtc_to_logging {
 public:
   log_webrtc_to_logging();
   ~log_webrtc_to_logging();
--- fubble/utils/logging/initialser.hpp
+++ fubble/utils/logging/initialser.hpp
@@ -1,11 +1,12 @@
 #ifndef LOGGING_INITIALISER_HPP
 #define LOGGING_INITIALISER_HPP
 
+#include "fubble/utils/export.hpp"
 #include "fubble/utils/logging/severity.hpp"
 
 namespace logging {
-void add_console_log(logging::severity severity_);
-void add_file_log(logging::severity severity_);
+void FUBBLE_PUBLIC add_console_log(logging::severity severity_);
+void FUBBLE_PUBLIC add_file_log(logging::severity severity_);
 } // namespace logging
 
 #endif
