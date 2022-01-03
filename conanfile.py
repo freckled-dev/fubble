f3fa595d6e98ddce610b5b7723c192e7bbfb5978
Markus Lanner <contact@markus-lanner.com>
2022-01-03 23:48:50 +0100

    google webrtc implementation cmake target added

--- conanfile.py
+++ conanfile.py
@@ -14,20 +14,29 @@ class FubbleConan(ConanFile):
     description = "Conferencing that works"
     topics = ("conference", "fubble", "video", "audio", "webrtc")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [False, True],
-            "treat_warnings_as_errors": [True, False],
-            "sanatize": [True, False], "qt_install": "ANY",
-            "enable_ui": [True, False], "meson_cross_file": "ANY"}
+    options = {
+        "shared": [False, True],
+        "fPIC": [True, False],
+        "treat_warnings_as_errors": [True, False],
+        "sanatize": [True, False],
+        "qt_install": "ANY",
+        "enable_ui": [True, False],
+        "meson_cross_file": "ANY"}
     # https://docs.conan.io/en/latest/reference/conanfile/attributes.html#default-options
-    default_options = {"shared": False, "qt_install": None, "enable_ui": True,
-            "meson_cross_file": None,
-            "restinio:asio": "boost",
-            # qt options
-            # "qt:openssl": False, "qt:with_mysql": False, "qt:with_pq": False, "qt:with_odbc": False, "qt:widgets": False,
-            # "qt:with_libalsa": False,
-            # qt submodules https://github.com/bincrafters/conan-qt/blob/testing/5.15.1/qtmodules.conf
-            # "qt:qtsvg": True, "qt:qtmultimedia": True, "qt:qtquickcontrols2": True, "qt:qtcharts": True,
-            "treat_warnings_as_errors": False, "sanatize": False}
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "qt_install": None,
+        "enable_ui": True,
+        "meson_cross_file": None,
+        "restinio:asio": "boost",
+        # qt options
+        # "qt:openssl": False, "qt:with_mysql": False, "qt:with_pq": False, "qt:with_odbc": False, "qt:widgets": False,
+        # "qt:with_libalsa": False,
+        # qt submodules https://github.com/bincrafters/conan-qt/blob/testing/5.15.1/qtmodules.conf
+        # "qt:qtsvg": True, "qt:qtmultimedia": True, "qt:qtquickcontrols2": True, "qt:qtcharts": True,
+        "treat_warnings_as_errors": False,
+        "sanatize": False}
     #exports_sources = "*", "!fubble/app/mock_qml_models*", "!.*"
     no_copy_source = True
 
@@ -66,9 +75,9 @@ class FubbleConan(ConanFile):
         #     # will not compile with less than visual studio 2019
         #     self.build_requires("qt/5.15.0@bincrafters/stable")
         if os_info.is_macos: # maybe even for windows, instead of the pkgconfig-lite "hack"
-            self.tool_requires('pkgconf/1.7.3')
+            self.tool_requires('pkgconf/[>=1.7]')
         if not tools.which('cmake'):
-            self.tool_requires('cmake/[>1.15]')
+            self.tool_requires('cmake/[>=1.15]')
 
     def requirements(self):
         self.requires("nlohmann_json/3.7.0")
@@ -139,6 +148,5 @@ class FubbleConan(ConanFile):
             '-DBOOST_BEAST_SEPARATE_COMPILATION',
             ]
         if self.settings.os == "Windows":
-            self.cpp_info.system_libs = [
-                'ole32.lib', 'dbgeng.lib',
-                ]
+            self.cpp_info.system_libs = ['ole32.lib', 'dbgeng.lib']
+
